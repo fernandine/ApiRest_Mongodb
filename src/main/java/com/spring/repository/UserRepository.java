e133e6836762f754bdef4589e07a6d6c277d0edb
Jean Fernandine <jfernandine@gmail.com>
2022-03-29 18:59:57 -0300

    User+repository+service

---
+++ src/main/java/com/spring/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/spring/services/UserService.java
@@ -0,0 +1,24 @@
+package com.spring.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.domain.User;
+import com.spring.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+	}
+	
+	
+}

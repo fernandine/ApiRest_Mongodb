05d1fbcdb4c56ebe56beec5fff1434c268055e19
Jean Fernandine <jfernandine@gmail.com>
2022-03-29 21:12:27 -0300

    padrao DTO

--- src/main/java/com/spring/resource/UserResource.java
+++ src/main/java/com/spring/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.spring.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.domain.User;
+import com.spring.dto.UserDto;
 import com.spring.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){	
+	public ResponseEntity<List<UserDto>> findAll(){	
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
